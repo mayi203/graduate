998400469d6a27f3f9b1fe69d390b41dcfdc60a1
李文飞 <wenfeili@163.com>
2014-10-28 21:33:32 +0800

    tb

--- LaGou/src/mayi/lagou/com/activity/MainActivity.java
+++ LaGou/src/mayi/lagou/com/activity/MainActivity.java
@@ -3,24 +3,15 @@ package mayi.lagou.com.activity;
 import mayi.lagou.com.R;
 import mayi.lagou.com.fragment.JobListFragment;
 import mayi.lagou.com.view.TabPageIndicator;
-import mayi.lagou.com.view.arcmenu.ArcMenu;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 public class MainActivity extends FragmentActivity {
 	private static String[] CONTENT;
-	private static final int[] ITEM_DRAWABLES = { R.drawable.composer_camera,
-			R.drawable.composer_music, R.drawable.composer_place,
-			R.drawable.composer_sleep, R.drawable.composer_thought,
-			R.drawable.composer_with };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -35,27 +26,6 @@ public class MainActivity extends FragmentActivity {
 
 		TabPageIndicator indicator = (TabPageIndicator) findViewById(R.id.indicator);
 		indicator.setViewPager(pager);
-
-		ArcMenu arcMenu = (ArcMenu) findViewById(R.id.arc_menu);
-		initArcMenu(arcMenu, ITEM_DRAWABLES);
-	}
-
-	private void initArcMenu(ArcMenu menu, int[] itemDrawables) {
-		final int itemCount = itemDrawables.length;
-		for (int i = 0; i < itemCount; i++) {
-			ImageView item = new ImageView(this);
-			item.setImageResource(itemDrawables[i]);
-
-			final int position = i;
-			menu.addItem(item, new OnClickListener() {
-
-				@Override
-				public void onClick(View v) {
-					Toast.makeText(MainActivity.this, "position:" + position,
-							Toast.LENGTH_SHORT).show();
-				}
-			});
-		}
 	}
 
 	class GoogleMusicAdapter extends FragmentPagerAdapter {
--- LaGou/src/mayi/lagou/com/fragment/JobListFragment.java
+++ LaGou/src/mayi/lagou/com/fragment/JobListFragment.java
@@ -1,16 +1,24 @@
 package mayi.lagou.com.fragment;
 
 import mayi.lagou.com.R;
+import mayi.lagou.com.view.arcmenu.ArcMenu;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public final class JobListFragment extends Fragment {
 	private static final String KEY_CONTENT = "TestFragment:Content";
+	private static final int[] ITEM_DRAWABLES = { R.drawable.composer_camera,
+		R.drawable.composer_music, R.drawable.composer_place,
+		R.drawable.composer_sleep, R.drawable.composer_thought,
+		R.drawable.composer_with };
 	TextView text;
 
 	public static JobListFragment newInstance(String content) {
@@ -40,6 +48,8 @@ public final class JobListFragment extends Fragment {
 		View main = inflater.inflate(R.layout.f_job_list, null);
 		TextView text = (TextView) main.findViewById(R.id.txt);
 		text.setText(mContent);
+		ArcMenu arcMenu = (ArcMenu) main.findViewById(R.id.arc_menu);
+		initArcMenu(arcMenu, ITEM_DRAWABLES);
 		return main;
 	}
 
@@ -48,5 +58,21 @@ public final class JobListFragment extends Fragment {
 		super.onSaveInstanceState(outState);
 		outState.putString(KEY_CONTENT, mContent);
 	}
+	private void initArcMenu(ArcMenu menu, int[] itemDrawables) {
+		final int itemCount = itemDrawables.length;
+		for (int i = 0; i < itemCount; i++) {
+			ImageView item = new ImageView(getActivity());
+			item.setImageResource(itemDrawables[i]);
 
+			final int position = i;
+			menu.addItem(item, new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					Toast.makeText(getActivity(), "position:" + position,
+							Toast.LENGTH_SHORT).show();
+				}
+			});
+		}
+	}
 }
